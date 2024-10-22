eb63b1a405f1abc143066637deaed3ff7d747b6a
Khalil-Guedidi <guedidi.khalil@gmail.com>
2024-10-23 00:09:42 +0200

    wip: real nodes interface system

--- addons/mapmagic_godot/mapmagic_godot.gd
+++ addons/mapmagic_godot/mapmagic_godot.gd
@@ -1,9 +1,15 @@
 @tool
 extends EditorPlugin
 
-func _enter_tree() -> void:
-	add_custom_type("MapMagicTerrain", "Node3D", load("res://addons/mapmagic_godot/mapmagic_terrain_node.gd"), preload("res://icon.svg"))
+var dock
 
+func _enter_tree() -> void:
+	add_custom_type("MapMagicTerrain", "Node3D", preload("res://addons/mapmagic_godot/mapmagic_terrain_node.gd"), preload("res://icon.svg"))
+	
+	dock = preload("res://addons/mapmagic_godot/dock/nodes_editor.tscn").instantiate()
+	add_control_to_bottom_panel(dock, "Map Magic")
 
 func _exit_tree() -> void:
 	remove_custom_type("MapMagicTerrain")
+	remove_control_from_bottom_panel(dock)
+	dock.free()
